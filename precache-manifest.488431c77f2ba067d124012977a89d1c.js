df65f46f6ba872a42f475e5009af1442eaaecdf0
Serhii Buriachenko <buryachenko.s@gmail.com>
2020-05-13 00:18:52 +0300

    Updates

---
+++ precache-manifest.488431c77f2ba067d124012977a89d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42a295efc85ba82a5871212f89da7f6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "431571cce28783880459",
+    "url": "/goit-react-hw-01-components/static/css/main.8200b170.chunk.css"
+  },
+  {
+    "revision": "c62c8ee618b2a9389911",
+    "url": "/goit-react-hw-01-components/static/js/2.676e4a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.676e4a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431571cce28783880459",
+    "url": "/goit-react-hw-01-components/static/js/main.d77e9b79.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
